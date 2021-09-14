12e8932c0181a8153dd6adfb14930a8d361b9985
Nikhil-622 <jadhavnikhil622@gmail.com>
2021-09-14 19:27:33 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+<!-- <link rel="stylesheet" type="text/css" href="../css/index.css"> -->
+
+
+  <div class="bg">
+    <div class="center">
+      <div class="head">Welcome to Sparks Bank</div>
+      <ul style="padding:0">
+        <li class="operations"><a href="transfer_money.php"><button  >View All Users</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button>Trasnfer Money</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button  >View Transfer History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,34 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sparks Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+</head>
+
+
+<body>
+
+  <nav id="nav" class="nav">
+    <div class="logo">
+      <a href="index.php"><img src="img/logo.png" alt="logo" ></a>
+    </div>
+    <div class="menu">
+      <ul class="list-items">
+        <li><a href="index.php">Home</a></li>
+        <li><a target="blank"href="https://drive.google.com/file/d/1qmy6nLJz74XKdTRUkvuMVUwxHLEcayJM/view?usp=sharing">About</a></li>
+        <li><a href="contactme.php">Contact</a></li>
+      </ul>
+    </div>
+    
+  </nav>
